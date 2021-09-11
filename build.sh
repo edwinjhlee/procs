bce474f3da907ca67b831e2d1645eade01fe33da
edwinjhlee <edwin.jh.lee@gmail.com>
2021-09-11 18:56:39 +0800

    ...

--- build.sh
+++ build.sh
@@ -13,21 +13,6 @@ appname=procs
 # apt-get install mingw-w64
 # apt install gcc
 
-build_win(){
-    (
-        cd code
-        cross build --target x86_64-pc-windows-gnu --release
-    )
-    cp code/target/x86_64-pc-windows-gnu/release/${appname}.exe bin/${appname}.x64.exe
-    (
-        cd bin
-        xrc 7z
-        _7z a -v1000k ${appname}.x64.exe.7z ${appname}.x64.exe
-        ls ${appname}.x64.exe.7z* | wc -l | tr -cd '0-9' > ${appname}.x64.exe.7z
-        rm ${appname}.x64.exe
-    )
-}
-
 build_main(){
 
     local rust_target=${1:?rust_target}
@@ -68,5 +53,6 @@ main(){
     build_main x86_64-apple-darwin ${appname}.darwin.x64
 
     build_main x86_64-pc-windows-gnu ${appname}.x64.exe ${appname}.exe
+    # build_main aarch64-pc-windows-msvc ${appname}.arm64.exe ${appname}.exe
 }
 
